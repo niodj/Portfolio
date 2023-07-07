59fbb6414db65a9b8c8abe83c26fe53ceef6a9ec
AntonPotapenko <a0501856767@gmail.com>
2023-07-07 09:53:56 +0200

    finished props styler

---
+++ src/CurrentBankomat.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import {MoneyType} from "./App";
+import styled from "styled-components";
+
+type CurrentBankomatPropsType = {
+    item: MoneyType
+}
+export const CurrentBankomat = (props:CurrentBankomatPropsType) => {
+
+    return (
+      <Banknote color={props.item.banknotes==='Dollars' ? 'green' : 'blue'}>
+            <Name>{props.item.banknotes}</Name>
+            <Value>{props.item.value}</Value>
+            </Banknote>
+        );
+};
+
+const Banknote = styled.div`
+  background-color: ${props=> props.color}; 
+    width: 400px;
+    height: 200px;
+    margin: 10px;
+`
+
+// const green = styled.div`
+// background-color: green;
+//   width: 400px;
+//   height: 200px;
+//   margin: 10px;
+// `
+// const blue = styled.div`
+//   background-color: lightseagreen;
+//   width: 400px;
+//   height: 200px;
+//   margin: 10px;
+// `
+
+const Name = styled.div`
+display: flex;
+  justify-content: center;
+  font-size: 30px;
+`
+
+const Value = styled.div`
+display: flex;
+  justify-content: center;
+  font-size: 30px;
+  margin-top: 30px;
+  font-size: 90px;
+`
