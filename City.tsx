16a67e78bf23fb02cb8ac9cf51b135f7d3cdbaee
AntonPotapenko <a0501856767@gmail.com>
2023-07-08 15:04:32 +0200

    add RubDollar

---
+++ City.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {CurrentBankomat} from "./CurrentBankomat";
+import {MoneyType} from "./RubDollarApp";
+import styled from "styled-components";
+
+type CityPropsType = {
+    filteredMoney: MoneyType[] //встречаем денюжки
+}
+
+export const City = (props: CityPropsType) => {
+
+    const mapData = props.filteredMoney.map((item,idx) => (
+        <CurrentBankomat
+            key={idx}
+            item={item}
+           />
+        ))
+
+    return <div>
+        <Wrapper>{mapData}</Wrapper>
+        </div>
+    };
+
+const Wrapper = styled.div`
+display:flex;
+flex-direction: row;
+flex-size: 38px
+`;
