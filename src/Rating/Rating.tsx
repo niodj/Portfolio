d5623829f0af5c520c3f20709b202484e1c3959f
AntonPotapenko <a0501856767@gmail.com>
2023-07-09 09:00:09 +0200

    add startRating

--- src/Rating/Rating.tsx
+++ src/Rating/Rating.tsx
@@ -1,21 +1,31 @@
 import {useState} from "react";
 import styled from "styled-components";
+import { AiOutlineStar, AiFillStar } from "react-icons/ai";
 
-const RATINGS = [1, 2, 3];
+const RATINGS = [ 1, 2, 3, 4, 5];
 
-export function Rating(props: any) {
+export function Rating() {
     const [selectedRating, setSelectedRating] = useState(0)
 
        return (
         <div className='App'>
-            {RATINGS.map(rating => (
-                <Star selected={rating<=selectedRating} key={rating} onClick={() => setSelectedRating(rating)}>Star</Star>
+            <div>My rating {selectedRating}. Choose by words</div>
+                     <div>
+                        {RATINGS.map(item => (
+                <Star selected={item<=selectedRating} key={item} onClick={() => setSelectedRating(item)}>Star </Star>
             ))}
+            </div>
+            <div>or here</div>
+            <div>
+                {RATINGS.map(item => (
+                    <span key={item} onClick={() => setSelectedRating(item)}> {item<=selectedRating? <AiFillStar /> : <AiOutlineStar />} </span>
+                ))}
+            </div>
         </div>
     );
 }
 
 const Star = styled.span<{selected:boolean}>`
   
-  ${(props) => (props.selected && 'font-weight: bold')};
+  ${(props) => (props.selected ? 'font-weight: bold':null)};
 `
