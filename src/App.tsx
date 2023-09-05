b07dc9cea1519f0a912c71b2dc07c8146b60b16b
AntonPotapenko <a0501856767@gmail.com>
2023-09-05 21:18:00 +0200

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Header} from "./components/Header";
+import {Navbar} from "./components/Navbar";
+import {Sidebar} from "./components/Sidebar";
+import styled from "styled-components";
+import {Dialogs} from "./components/Dialogs";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Wrapper>
+     <Header />
+       <NavSideWrapper>
+     <Navbar />
+     <Sidebar />
+       </NavSideWrapper>
+</Wrapper>
   );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+
+`
+const NavSideWrapper = styled.div`
+display: flex;
+  flex-direction: row;
+  min-height: 100vh;
+  margin-top: 5px ;
+`
+
+const  SidebarWrapper = styled.div`
+
+`
---
+++ src/components/Dialogs.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Dialogs = () =>{
+
+    return(
+        <Wrapper>Dialogs</Wrapper>
+    )
+}
+
+const Wrapper = styled.div`
+
+`
