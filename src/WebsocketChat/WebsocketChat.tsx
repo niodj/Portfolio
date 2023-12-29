ced8557ae04bfbcf7676862770da3cee7c93b622
Anton Potapenko <a0501856767@gmail.com>
2023-12-29 13:26:08 +0100

    add clear chat

--- src/WebsocketChat/WebsocketChat.tsx
+++ src/WebsocketChat/WebsocketChat.tsx
@@ -1,56 +1,77 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
+import s from './websocket.module.css'
+import Input from "@mui/material/Input/Input";
+import Button from "@mui/material/Button";
 
-const socket = io("http://localhost:4444");
-
-
-interface ChatMessage {
+ type ChatMessage ={
   userName: string;
   message: string;
 }
 
 export const WebsocketChat = () => {
+  const socket = io("http://localhost:4444");
+  const [userName, setUserName] = useState('Admin');
   const [message, setMessage] = useState("");
   const [chat, setChat] = useState<ChatMessage[]>([]);
 
+const clearChat = () => {
+  socket.emit("clearChat");
+  };
+
   useEffect(() => {
+    socket.on("chatCleared", () => {
+      setChat([]);
+    });
+    
+      socket.on("chatData", ({ messageHistory, newMessage }) => {
+        setChat(messageHistory);
+      });
+
     socket.on("message", (payload: ChatMessage) => {
       setChat((prevChat) => [...prevChat, payload]);
     });
-
-    // Clean up the socket listener when the component unmounts
     return () => {
+      socket.off("messageHistory");
       socket.off("message");
     };
-  }, []); // Empty dependency array means this effect runs once when the component mounts
+  }, []);
 
-  const sendMessage = (e: React.FormEvent) => {
-    e.preventDefault();
-    socket.emit("message", { userName:'user', message });
+const sendMessage = (e: React.FormEvent) => {
+  e.preventDefault();
+  if (message.trim() !== "") {
+    socket.emit("message", { userName, message });
     setMessage("");
-  };
+  }
+};
 
   return (
-    <div className='App'>
-      <h1>Welcome to chatter</h1>
-      <form onSubmit={sendMessage}>
-        <input
-          type='text'
-          name='message'
-          placeholder='Type message'
-          value={message}
-          onChange={(e) => {
-            setMessage(e.target.value);
-          }}
-          required
-        />
-        <button type='submit'>Send</button>
-      </form>
-      {chat.map((payload, index) => (
-        <h4 key={index}>
-          {payload.userName}: <span>{payload.message}</span>
-        </h4>
-      ))}
+    <div className={s.wrapper}>
+      <h3>Welcome to chatter</h3>
+      <textarea
+        value={chat
+          .map((payload) => `${payload.userName}: ${payload.message}\n`)
+          .join("")}
+        readOnly
+        rows={10}
+        cols={50}
+      />
+      <Input
+        value={userName}
+        onChange={(e) => {
+          setUserName(e.target.value);
+        }}
+      ></Input>
+      :
+      <Input
+        placeholder='Type message'
+        value={message}
+        onChange={(e) => {
+          setMessage(e.target.value);
+        }}
+      ></Input>
+      <Button onClick={sendMessage}>Send</Button>
+      <Button onClick={clearChat}>Send</Button>
     </div>
   );
 };
